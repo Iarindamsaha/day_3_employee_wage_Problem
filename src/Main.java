339053cb6ca299f015b17d63efbda22426d8f891
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 12:28:38 +0530

    Added Daily Wage Calculation Feature

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,24 @@ public class Main {
         System.out.println("Welcome To Employee Wage Computation Program");
         System.out.println("............................................");
         System.out.println(" ");
-        int isPresent = 0;
-        int isAbsent = 1;
+
+        int isPresent = 1;
+        int isAbsent = 0;
+        int dailyWage = 20;
+        int dailyWorkHour;
+        int dailySalary;
         int checkAttendance =(int)((Math.random()*10)%2);
 
         if (checkAttendance == isPresent){
             System.out.println("The Employee is Present");
-
+            dailyWorkHour=8;
         }
         else{
             System.out.println("The Employee is Absent");
+            dailyWorkHour=0;
+        }
+        dailySalary = dailyWage * dailyWorkHour;
+        System.out.println("Daily Salary is ="+dailySalary);
 
         }
-    }
 }
