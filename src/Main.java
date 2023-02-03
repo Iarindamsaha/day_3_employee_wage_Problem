9d89f2a699de47eb722885002c249fe7fdd5a16c
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 13:38:57 +0530

    Added Conditional Feauture in Monthly Wage Calculation

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,57 @@ public class Main {
         System.out.println("Welcome To Employee Wage Computation Program");
         System.out.println("............................................");
         System.out.println(" ");
+
         final int isPresent = 1;
         final int isAbsent = 0;
         final int isPartTime =2;
 
         int daily_wage_per_hour = 100;
+        int max_hour_in_month=100;
         int working_day = 20;
-        int Total_salary = 0;
-        int dailyWorkHour;
+        //while loop vars
+        int Total_salary=0;
+        int total_working_hr=0;
+        int total_working_days=1;
+        int total_emp_hr=0;
         int dailySalary;
 
-        for (int day = 0; day <= working_day; day++) {
-            int return_value = 0;
+        while (total_emp_hr <= max_hour_in_month && total_working_days < working_day){
+            int return_value;
             int checkAttendance =(int)((Math.random()*10)%3);
             switch(checkAttendance){
                 case isPresent: {
-                    //System.out.println("The Employee is Present");
-                    dailyWorkHour = 8;
+                    int dailyWorkHour = 8;
                     return_value=dailyWorkHour;
                     break;
                 }
                 case isPartTime: {
-                    //System.out.println("The Employee is Present But Part Time..");
-                    dailyWorkHour = 5;
+                    int dailyWorkHour = 5;
                     return_value=dailyWorkHour;
                     break;
                 }
                 default: {
-                    //System.out.println("The Employee is Absent");
-                    dailyWorkHour=0;
+                    int dailyWorkHour=0;
                     return_value=dailyWorkHour;
                     break;
                 }
             }
-            dailySalary = daily_wage_per_hour * dailyWorkHour;
-            System.out.println("Daily Salary= "+dailySalary);
-            Total_salary=Total_salary+dailySalary;
+            total_emp_hr=total_emp_hr+return_value;
+
+            /*if(total_emp_hr>max_hour_in_month){
+                total_emp_hr=total_emp_hr-return_value;
+                break;
+            }
+            */
+
+            dailySalary = return_value*daily_wage_per_hour;
+            System.out.println("Daily Salary="+dailySalary);
+
+
+            Total_salary = Total_salary + dailySalary;
+            total_working_days++;
         }
-        System.out.println("..............................");
-        System.out.println("Monthly Salary ="+Total_salary);
+        System.out.println("..........................");
+        System.out.println("Total Salary:"+Total_salary);
     }
 }
