6b84f11b06bd05f9765c5b9bb3ba986d66ae8459
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 12:23:05 +0530

    Added Attendence Feature

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Employee Wage Computation Program");
+        System.out.println("............................................");
+        System.out.println(" ");
+        int isPresent = 0;
+        int isAbsent = 1;
+        int checkAttendance =(int)((Math.random()*10)%2);
+
+        if (checkAttendance == isPresent){
+            System.out.println("The Employee is Present");
+
+        }
+        else{
+            System.out.println("The Employee is Absent");
+
+        }
     }
 }
