c6b03465059472d262c6ddcc93e19d9ef02ecdcb
Arindam Saha <asaha15071998@gmail.com>
2023-02-11 20:13:12 +0530

    Added Feature to Control Wages As per Wish

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,13 @@ public class Main {
     public static final int isPresent = 1;
     public static final int isPartTime = 2;
 
-    int Total_salary_company1 = 0;
-    int Total_salary_company2 = 0;
 
+    public void employeeWageCalculation(String employeeName, String companyName, int dailyWage, int max_hour_in_month, int working_day){
 
-    public void employeeWageCalculation_1(){
-
-        final int isPresent = 1;
-        final int isPartTime = 2;
-        int max_hour_in_month = 70;
-        int working_day = 30;
-
-        String companyName = "Google";
-        int dailyWage = 1200;
+        System.out.println(" ");
 
         //while loop vars
-
+        int Total_salary=0;
         int total_working_days=1;
         int total_emp_hr=0;
         int dailySalary;
@@ -48,71 +39,20 @@ public class Main {
 
             dailySalary = return_value*dailyWage;
 
-            Total_salary_company1 = Total_salary_company1 + dailySalary;
+            Total_salary = Total_salary + dailySalary;
             total_working_days++;
         }
-        System.out.println("Company Name : " + companyName);
-        System.out.println("Total Salary:" + Total_salary_company1);
-        System.out.println("........................");
-
-    }
-
-    public void employeeWageCalculation_2(){
-        final int isPresent = 1;
-        final int isPartTime = 2;
-        int max_hour_in_month = 65;
-        int working_day = 15;
-
-        String companyName = "Microsoft";
-        int dailyWage = 1000;
-
-        //while loop vars
-
-        int total_working_days=1;
-        int total_emp_hr=0;
-        int dailySalary;
-
-        while (total_emp_hr <= max_hour_in_month && total_working_days < working_day){
-
-            int return_value;
-            int checkAttendance =(int)((Math.random()*10)%3);
-            switch(checkAttendance){
-                case isPresent: {
-                    int dailyWorkHour = 8;
-                    return_value=dailyWorkHour;
-                    break;
-                }
-                case isPartTime: {
-                    int dailyWorkHour = 5;
-                    return_value=dailyWorkHour;
-                    break;
-                }
-                default: {
-                    int dailyWorkHour=0;
-                    return_value=dailyWorkHour;
-                    break;
-                }
-            }
-            total_emp_hr=total_emp_hr+return_value;
-
-            dailySalary = return_value*dailyWage;
-
-            Total_salary_company2 = Total_salary_company2 + dailySalary;
-            total_working_days++;
-        }
-        System.out.println("Company Name : " + companyName);
-        System.out.println("Total Salary:" + Total_salary_company2);
+        System.out.println("Employee Name : " + employeeName + "\n" + "Company Name : " + companyName);
+        System.out.println("Total Salary:" + Total_salary);
         System.out.println("........................");
     }
-
-
     public static void main(String[] args) {
 
         System.out.println("Welcome To Employee Wage Computation Program");
         System.out.println("............................................");
         Main obj = new Main();
-        obj.employeeWageCalculation_1();
-        obj.employeeWageCalculation_2();
+        obj.employeeWageCalculation("ABC" , "Microsoft" , 900, 65, 30);
+        obj.employeeWageCalculation("XYZ" , "Google" , 800, 80, 27 );
 
     }
 }
